f77a4f157f7fbfce7744da0365c5696699435e19
N1KUSO <kisilnikolay1356@gmail.com>
2024-01-29 13:03:29 +0300

    fix

--- 1 курс 2 семестр/ДМ/Лабораторная работа №1/Вариант 10.c
+++ 1 курс 2 семестр/ДМ/Лабораторная работа №1/Вариант 10.c
@@ -1,91 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void inputArray(int *a, const size_t n) {
+    for (size_t i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 }
 
-void selectionsort(int *a, const int size) {
-    for (int i = 0; i < size - 1; i++) {
-        int minpos = i;
-        for (int j = i + 1; j < size; j++)
-            if (a[j] < a[minpos])
-                minpos = j;
-        swap(&a[i], &a[minpos]);
-    }
+void outputArray(int *a, const int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
 }
 
-void mergearrays(int a[], int b[], int c[], int sizea, int sizeb, int *sizec) {
-    int i = 0, j = 0, k = 0;
 
-    while (i < sizea && j < sizeb) {
-        if (a[i] < b[j]) {
-            c[k++] = a[i++];
-        } else if (a[i] > b[j]) {
-            j++;
-        } else {
-            i++;
-            j++;
+void uniqueA(int *a, int *b, int n_a, int n_b, int *c, int *n_c) {
+    for (int i = 0; i < n_a; i++) {
+        int is_contains = 0;
+        for(size_t j = 0; j < n_b; j++) {
+            if (a[i] == b[j]) {
+                is_contains = 1;
+            }
+        }
+        if(is_contains == 0) {
+            c[*n_c] = a[i];
+            (*n_c)++;
         }
     }
-
-    while (i < sizea) {
-        c[k++] = a[i++];
-    }
-    
-    *sizec = k;
-}
-
-int contains(int arr[], int size, int element) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == element) {
-            return 1;
-        }
-    }
-    return 0;
 }
 
 int main() {
-    int sizea, sizeb;
-    
-    printf("размер массива a: ");
-    scanf("%d", &sizea);
-    int a[sizea];
-    printf("элементы массива a: ");
-    for (int i = 0; i < sizea; i++) {
-        scanf("%d", &a[i]);
-    }
-    
-    printf("размер массива b: ");
-    scanf("%d", &sizeb);
-    int b[sizeb];
-    printf("элементы массива b: ");
-    for (int i = 0; i < sizeb; i++) {
-        scanf("%d", &b[i]);
-    }
+    int n_a;
+    scanf("%d", &n_a);
+    int a[n_a];
+    inputArray(a, n_a);
 
-    int c[sizea]; 
-    int sizec = 0;
+    int n_b;
+    scanf("%d", &n_b);
+    int b[n_b];
+    inputArray(b, n_b);
 
-    for (int i = 0; i < sizea; i++) {
-        if (!contains(b, sizeb, a[i])) {
-            c[sizec++] = a[i];
-        }
-    }
-    
-    if (sizec == 0) {
-        printf("все элементы массивов совпадают");
-        return 0;
-    }
-    
-    selectionsort(c, sizec);
+    int n_c = 0;
+    int c[n_c];
 
-    printf("массив c: ");
-    for (int i = 0; i < sizec; i++) {
-        printf("%d ", c[i]);
-    }
+    uniqueA(a, b, n_a, n_b, c, &n_c);
+
+    outputArray(c, n_c);
 
     return 0;
 }
